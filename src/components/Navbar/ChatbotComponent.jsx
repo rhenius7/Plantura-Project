aa133209335bb2ee40cb73fcb976976bb9ca10fb
RHENIUS  D <124780835+rhenius7@users.noreply.github.com>
2024-11-22 08:24:22 +0530

    Update ChatbotComponent.jsx

--- src/components/Navbar/ChatbotComponent.jsx
+++ src/components/Navbar/ChatbotComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ChatbotComponent = () => {
+const ChatbotComponent = ({ src }) => {
   return (
     <div style={styles.container}>
       <iframe
@@ -8,7 +8,7 @@ const ChatbotComponent = () => {
         width="350"
         height="430"
         allow="microphone;"
-        src="https://console.dialogflow.com/api-client/demo/embedded/8d423b83-e863-4dac-a4b1-7ee2d87b745a"
+        src={src}
         style={styles.iframe}
       ></iframe>
     </div>
@@ -18,16 +18,16 @@ const ChatbotComponent = () => {
 const styles = {
   container: {
     position: "fixed",
-    top: "50%", 
-    right: "0", 
-    transform: "translateY(-50%)", 
+    top: "50%",
+    right: "0",
+    transform: "translateY(-50%)",
     zIndex: 1000,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     padding: "10px",
-    backgroundColor: "#fff", 
-    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.3)", 
+    backgroundColor: "#fff",
+    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.3)",
     borderRadius: "8px",
   },
   iframe: {
